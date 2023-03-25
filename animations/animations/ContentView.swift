609819c2a671dd8bdabba47f23e2db95f636b7b0
personal <darkfabiotm@gmail.com>
2023-03-25 20:07:00 -0300

    :recycle: fix[animation]add new animationst

--- animations/animations/ContentView.swift
+++ animations/animations/ContentView.swift
@@ -13,18 +13,42 @@ struct ContentView: View {
     @State private var animationAmount2 = 1.0
     
     @State private var animationAmount3d = 0.0
+    
+    let letters = Array("Animation SwiftUI")
+        @State private var enabled = false
+        @State private var dragAmount = CGSize.zero
     var body: some View {
+        
         VStack {
             Stepper("Escala", value: $animationAmount2.animation(), in: 1...10)
             Spacer()
-            
+
+            HStack(spacing: 0) {
+                ForEach(0..<letters.count, id: \.self) {num in
+                    Text(String(letters[num]))
+                        .padding(5)
+                        .font(.title)
+                        .background(enabled ? .blue : .red)
+                        .offset(dragAmount)
+                        .animation(.default.delay(Double(num) / 20), value: dragAmount)
+                }
+                
+            }
+            .gesture(
+                DragGesture()
+                    .onChanged{dragAmount = $0.translation}
+                    .onEnded{_ in
+                        dragAmount = .zero
+                        enabled.toggle()
+                    }
+            )
             Button("3d") {
                 withAnimation(
                     .interpolatingSpring(stiffness: 5, damping: 1))
                 {
                     animationAmount3d += 360
                 }
-                
+
             }
             .padding(50)
             .background(.green)
@@ -34,7 +58,7 @@ struct ContentView: View {
                 .degrees(
                     animationAmount3d),
                 axis: (x: 0, y:1, z: 0))
-            
+
             Button("Scale") {
                 animationAmount2 += 1
             }
@@ -43,7 +67,7 @@ struct ContentView: View {
             .foregroundColor(.white)
             .clipShape(Circle())
             .scaleEffect(animationAmount2)
-            
+
             Button("repeat forever") {
                 //            animationAmount += 1
             }
